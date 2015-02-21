0187edafa7ea4399c68d5ab389d42ee98f5ccf32
David Dworken <david@daviddworken.com>
2015-02-21 01:33:59 -0500

    Refactored code and added support for 6 motors/servos
    Added support for 6 servos instead of 2
    Added support for 6 motors instead of 5
    Added in additional error checking in controlMotor method
    Replaced #define statements for motor pins with arrays (const arrays stored in PROGMEM so no increased ram usage, don't worry)
    Reduced memory usage with F() to store strings in flash
    
    tl;dr: Decreased ram, increased error checking, and supported more servos/motors

--- ACTUAL CODE/DA_MOTOR_CONTROLLER/DA_MOTOR_CONTROLLER.ino
+++ ACTUAL CODE/DA_MOTOR_CONTROLLER/DA_MOTOR_CONTROLLER.ino
@@ -1,72 +1,22 @@
 #include <Servo.h>
-//###################################
-//###################################
-// Constants
-//###################################
-//###################################
-
-//************************************
-// Miscellaneous
-//************************************
-
-#define FORWARD HIGH
-#define BACKWARD LOW
-
-//************************************
-// Motor pins
-//************************************
 
 #define NUM_MOTORS 6
+#define NUM_SERVOS 6
 
-//Motor thruster pins
-// ADJUST THESE WHEN PINS ARE FINALIZED
-#define MT1t_PIN 11
-#define MT2t_PIN 9
-#define MT3t_PIN 7
-#define MT4t_PIN 5
-#define MT5t_PIN 2
-#define MT6t_PIN 12 // 6 Controls vertical motors
-int motor_thrust_pins[] = {MT1t_PIN, MT2t_PIN, MT3t_PIN, MT4t_PIN, MT5t_PIN, MT6t_PIN};
+const int dirPins[NUM_MOTORS] PROGMEM = {42,44,46,48,50,52};
+const int speedPins[NUM_MOTORS] PROGMEM = {2,3,4,5,6,7};
+const int servoPins[NUM_SERVOS] PROGMEM = {8,9,10,11,12,13};
 
-//Motor direction pins
-// ADJUST THESE WHEN PINS ARE FINALIZED
-#define MT1d_PIN 10
-#define MT2d_PIN 1
-#define MT3d_PIN 60
-#define MT4d_PIN 4
-#define MT5d_PIN 1
-#define MT6d_PIN 1
-int motor_dir_pins[] = {MT1d_PIN, MT2d_PIN, MT3d_PIN, MT4d_PIN, MT5d_PIN, MT6d_PIN};
-
-//************************************
-// Servo Values (unsigned 0-90 integers)
-//************************************
-
-#define SERVO_1_PIN 41
-#define SERVO_2_PIN 39
-
+Servo servo0;
 Servo servo1;
 Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
 
-//###################################
-//###################################
-// Global Variables
-//###################################
-//###################################
+int motor_values[] = {0, 0, 0, 0, 0, 0};
+int servo_values[] = {0, 0, 0, 0, 0, 0};
 
-//************************************
-// Motor Values (signed 8-bit integers)
-//************************************
-
-int motor_values[] = {0, 0, 0, 0, 0};
-int servo_values[] = {0, 0};
-
-
-//###################################
-//###################################
-// Code!
-//###################################
-//###################################
 
 void setup() {
   //*********************************
@@ -78,28 +28,20 @@ void setup() {
   Serial.println("Serial initialized");
 
   // Set thrust pins to output
-  pinMode(13, OUTPUT);
-  pinMode(MT1t_PIN, OUTPUT);
-  pinMode(MT2t_PIN, OUTPUT);
-  pinMode(MT3t_PIN, OUTPUT);
-  pinMode(MT4t_PIN, OUTPUT);
-  pinMode(MT5t_PIN, OUTPUT);
-  pinMode(MT6t_PIN, OUTPUT);
-  Serial.println("Motors initialized");
+  for(int i = 0; i < NUM_MOTORS; i++){
+    pinMode(dirPins[i],OUTPUT);
+    pinMode(speedPins[i],OUTPUT);
+  }
+  Serial.println(F("All Motor Pins Initialized"));
   
-//  // Set direction pins to output
-  pinMode(13, OUTPUT);
-  pinMode(MT1d_PIN, OUTPUT);
-  pinMode(MT2d_PIN, OUTPUT);
-  pinMode(MT3d_PIN, OUTPUT);
-  pinMode(MT4d_PIN, OUTPUT);
-  pinMode(MT5d_PIN, OUTPUT);
-  pinMode(MT6d_PIN, OUTPUT);
-  Serial.println("Direction initialized");
   
 //  // Attach servos
-  servo1.attach(SERVO_1_PIN);
-  servo2.attach(SERVO_2_PIN);
+  servo0.attach(servoPins[0]);
+  servo1.attach(servoPins[1]);
+  servo2.attach(servoPins[2]);
+  servo3.attach(servoPins[3]);
+  servo4.attach(servoPins[4]);
+  servo5.attach(servoPins[5]);
   
   //*********************************
   // Test Systems
@@ -114,35 +56,42 @@ void setup() {
   delay(500);
   digitalWrite(13, LOW);
     
-  //Test motors
-  for (int i = 0; i < NUM_MOTORS; i++) {
-    Serial.print("Testing motor ");
-    Serial.println(i);
-    
-    for (int j = 0; j < 256; j++) {
-      analogWrite(motor_thrust_pins[i], j);
-      delay(1);
-    }
-    
-    for (int j = 255; j >= 0; j--) {
-      analogWrite(motor_thrust_pins[i], j);
-      delay(1);
+  Serial.println(F("Testing motors"));
+  for(int motor = 0; motor < NUM_MOTORS; motor++){
+    Serial.print(F("Testing motor: "));
+    Serial.println(motor);
+    for(int speed = -100; speed<100; speed++){
+      controlMotor(motor,speed);
     }
   }
   
   Serial.println("Testing servos...");
-  //Test servos
-  //  #1
+  servo0.write(30);
+  delay(500);
+  servo0.write(50);
+  
   servo1.write(70);
   delay(500);
   servo1.write(100);
-  //  #2
+  
   servo2.write(30);
   delay(500);
   servo2.write(50);
+  
+  servo3.write(30);
+  delay(500);
+  servo3.write(50);
+  
+  servo4.write(30);
+  delay(500);
+  servo4.write(50);
+  
+  servo5.write(30);
+  delay(500);
+  servo5.write(50);
   Serial.println("Servos tested");
   
-} // End setup
+}
 
 void loop() {
   
@@ -255,8 +204,6 @@ void loop() {
 boolean controlMotor(int motorNum, int motorSpeed){   //
   int forward = 1;                                    //IDK if these constants are right, so constants so that i can change easily :D
   int reverse = 0;
-  int dirPins[] = {42,44,46,48,50,52};
-  int speedPins[] = {2,3,4,5,6,7};
   int direction = 0;
   if(motorSpeed > 0){                                 //if motor speed, is positive then direction should be forward
     if(motorSpeed <= 100){                            //if motor speed, is also less than 100 then it is in the correct range and should go forward
@@ -284,6 +231,4 @@ boolean controlMotor(int motorNum, int motorSpeed){   //
   Serial.println(motorNum);
   analogWrite(speedPins[motorNum],speed);
   digitalWrite(dirPins[motorNum],direction);
-}
-
 }
